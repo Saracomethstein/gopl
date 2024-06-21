07f39483e4c7e51f30f0f0d19df2f7f0ad44e26c
Baratheon Alaina <barathea@va-b5.kzn.21-school.ru>
2024-06-21 17:12:13 +0300

    new file system

---
+++ ch1/fetch/fetch.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	fetch()
+}
+
+// fetch выводит ответ на запрос по заданному URL
+func fetch() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(addPrefix(url))
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reader %s: %b\n", url, err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%s\n", b)
+	}
+}
+
+func addPrefix(url string) (result string) {
+	var prefix string = "http://"
+
+	if url[:7] != "http://" {
+		result = prefix + url
+	} else {
+		return url
+	}
+
+	return result
+}
