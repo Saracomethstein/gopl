84130d83c3f7ca747554b6ee01a5229e393b7f70
Saracomethstein <adel.sevastianov@yandex.com>
2024-07-07 14:22:42 +0300

    add tasks 3.10-3.12

--- ch3/comma/comma.go
+++ ch3/comma/comma.go
@@ -1,11 +1,9 @@
 package main
 
-import "fmt"
-
-func main() {
-	var str string = "1234511121"
-	fmt.Printf("%s\n", comma(str))
-}
+import (
+	"bytes"
+	"sort"
+)
 
 func comma(s string) string {
 	n := len(s)
@@ -15,4 +13,72 @@ func comma(s string) string {
 	}
 
 	return comma(s[:n-3]) + "," + s[n-3:]
+}
+
+func commaNonRecursion(s string) string {
+	var buf bytes.Buffer
+	n := len(s)
+	start := n % 3
+	if start > 0 {
+		buf.WriteString(s[:start])
+	}
+
+	for i := start; i < n; i += 3 {
+		if buf.Len() > 0 {
+			buf.WriteByte(',')
+		}
+		buf.WriteString(s[i : i+3])
+	}
+
+	return buf.String()
+}
+
+func commaFloat(s string) string {
+	var buf bytes.Buffer
+	n := len(s)
+	start := n % 3
+	end := 0
+
+	if start > 0 {
+		buf.WriteString(s[:start])
+	}
+
+	for i := start; i < n; i += 3 {
+		if buf.Len() > 0 {
+			if s[i] == '.' {
+				end = i
+				break
+			}
+			buf.WriteByte(',')
+		}
+		buf.WriteString(s[i : i+3])
+	}
+
+	for i := end; i < len(s); i++ {
+		buf.WriteByte(s[i])
+	}
+
+	return buf.String()
+}
+
+func isAnagram(str1, str2 string) bool {
+	if str1 == str2 {
+		return false
+	}
+
+	if len(str1) != len(str2) {
+		return false
+	}
+
+	if sortString(str1) == sortString(str2) {
+		return true
+	}
+
+	return false
+}
+
+func sortString(s string) string {
+	arr := []byte(s)
+	sort.Slice(arr, func(i, j int) bool { return arr[i] < arr[j] })
+	return string(arr)
 }
---
+++ ch3/comma/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var str1 string = "table"
+	var str2 string = "bleat"
+	fmt.Printf("%v\n", isAnagram(str1, str2))
+}
